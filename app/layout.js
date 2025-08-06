7451e33cdde6a7c472f71faece7a32fd564c3d49
Digital Esposure Online Services <131973180+Deos2023@users.noreply.github.com>
2025-08-06 16:58:10 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import ClientLayout from './components/ClientLayout';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -12,28 +13,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head>
-        {/* ✅ Google Tag Manager script (executes after hydration) */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TM8QFFPL');
-            `,
-          }}
-        />
+      <head>
+        {/* ✅ Google Tag Manager script - placed as high in head as possible */}
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TM8QFFPL');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
-        {/* ✅ Google Tag Manager noscript (for JS-disabled browsers) */}
+        {/* ✅ Google Tag Manager noscript - placed immediately after opening body tag */}
         <noscript>
-         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TM8QFFPL"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TM8QFFPL"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
         </noscript>
+
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
